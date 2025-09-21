3382ef3357a26d14598cf7e63a5bdcc17d0ef726
Hajime <chen2346897@gmail.com>
2025-09-21 17:02:09 +0800

    20250921_補上後台首頁

---
+++ Meow.web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Meow.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Policy = "AdminOnly")] // 你已在 Program.cs 設定 AdminOnly Policy
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
